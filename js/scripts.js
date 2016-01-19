53eb58b18b427dc72a6a65b7a64b057bf219a45a
alissa paluska and nic netzel <alissa.paluska@gmail.com>
2016-01-19 13:53:39 -0800

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
-var side1;
-var side2;
-var side3;
-
 var isTriangle = function(side1, side2, side3){
   if (defTriangle(side1, side2, side3)){
-    console.log(typeTriangle(side1, side2, side3));
     return typeTriangle(side1, side2, side3); //return value of triangle sides
   }else{
     return "Oops, please try again. Entry was incorrect.";  //error message goes here
@@ -12,7 +7,7 @@ var isTriangle = function(side1, side2, side3){
 };
 
 var typeTriangle = function(side1, side2, side3){
-  if (side1 === side2 && side1 === side3) {
+  if (side1 === side2 && side1 === side3 && side2 === side3) {
     return "equilateral";
 } else if (side1 === side2 && !(side1 = side3) || side2 === side3  && !(side1 === side3) || side1 === side3 && !(side2 === side3)){
     return "isoceles";
@@ -23,13 +18,24 @@ var typeTriangle = function(side1, side2, side3){
 
 // Define what a triangle is START
 var defTriangle = function(side1,side2,side3){
- return side1 <= (side2 + side3) && side2 <= (side3 + side1) && side3<= (side1 + side2);
+    return side1 <= (side2 + side3) && side2 <= (side3 + side1) && side3 <= (side1 + side2);
   };
 // Define what a triangle is END
 
 
 $(document).ready(function() {
 
+  $("#numberInput").submit(function(event){
 
+  var side1 = parseInt($("input#side1").val());
+  var side2 = parseInt($("input#side2").val());
+  var side3 = parseInt($("input#side3").val());
+
+  var result = isTriangle(side1,side2, side3);
+
+  $("#results").text(result);
+
+    event.preventDefault();
+  });
 
 });
